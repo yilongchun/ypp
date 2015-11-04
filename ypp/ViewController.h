d5a6d49242ad4f3e89bf83fd63b9922bf21a2254
yilongchun <5115413@qq.com>
2015-11-04 12:01:07 +0800

    Initial Commit

---
+++ ypp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ypp
+//
+//  Created by haidony on 15/11/4.
+//  Copyright © 2015年 weyida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ypp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ypp
+//
+//  Created by haidony on 15/11/4.
+//  Copyright © 2015年 weyida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
